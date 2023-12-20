276d4c799ddc3c4c9883ef21eb13de9eb284248d
vincentkenutama <vincentkenutama.2021@student.uny.ac.id>
2023-12-20 14:26:14 +0700

    second

---
+++ Foam2D/Foam2D/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Foam2D
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Foam2D/Foam2D/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Foam2D
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
