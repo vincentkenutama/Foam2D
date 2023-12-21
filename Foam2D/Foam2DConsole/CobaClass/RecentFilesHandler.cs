d0ff2d7ed248a2b9e1dcd71e6bbacfa7dfc923f0
vincentkenutama <vincentkenutama.2021@student.uny.ac.id>
2023-12-21 19:37:07 +0700

    json reader

---
+++ Foam2D/Foam2DConsole/CobaClass/RecentFilesHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Foam2DConsole.CobaClass
+{
+    internal class RecentFilesHandler
+    {
+        private string? file_path;
+
+        private void SetPath(string path)
+        {
+
+            file_path = $@"{path}";
+        }
+
+        public void GetRecent(string path)
+        {
+            SetPath(path);
+            if(File.Exists(file_path))
+            {
+                RecentFiles.AddFilePath(file_path);
+
+                Console.WriteLine("file exist");
+
+                using (StreamReader sr = File.OpenText(file_path))
+                {
+
+                    string s;
+                    while((s = sr.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(s);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("file doesn't exist");
+            }
+
+            
+        }
+
+
+    }
+}
---
+++ Foam2D/Foam2DConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Foam2DConsole.CobaClass;
+
+namespace Foam2DConsole
+{
+    class Foam2DConsole
+    {
+        public static int Main(string[] args)
+        {
+            //RecentFilesHandler recentFilesHandler = new RecentFilesHandler();
+
+            //string path = @"D:\abc.txt";
+            //recentFilesHandler.GetRecent(path);
+
+            //path = @"D:\purepursuit.txt";
+            //recentFilesHandler.GetRecent(path);
+
+            //RecentFiles.GetFilePath();
+            //RecentFiles.SaveToJson(@"D:\coba.json");
+            ////Console.WriteLine(RecentFiles.file_path[0]);
+            ////Console.WriteLine("coba");
+            ///
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.ReadKey();
+
+            /*ieldAccessException.*/
+
+            return 0;
+        }
+    }
+}
